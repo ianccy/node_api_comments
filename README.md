831ddadd7cbde2f39f34adca1625e50e97a2e1c9
Ianpig <chu2815@hotmail.com>
2018-10-13 10:12:05 +0800

    Build express api server

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # node_api_comments
+It's RESTful API for sample comment board.
+
+## mongodb
+you need create your mongodb to store deta.
+
+## npm install
+install node_modules
+
+## creare your config.json
+```
+{
+  "DB_URL" : "mongodb://dbuser:dbpassword@ds131323.mlab.com:xxxxx/xxxxxxx"
+}
+```
+
+## start server
+http://localhost:3000
+
+## start server
+npm start
+
+### Get API
+GET http://localhost:3000/comments
+
+### Post API
+POST http://localhost:3000/comments
+{ 
+  name:xxx , 
+  time: yyyy/mm/dd-hour:min:sec, 
+  content: xxx
+}
+
+### Delete API
+DELETE http://localhost:3000/comments/:id
+
+### Update API
+PUT http://localhost:3000/comments/:id
+{ 
+  name:xxx or 
+  time: yyyy/mm/dd-hour:min:sec or 
+  content: xxx 
+}
---
+++ index.js
@@ -0,0 +1,73 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+const ObjectID = require('mongodb').ObjectID;
+
+const { DB_URL: url } = require('./config.json');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/comments', (req, res) => {
+  db.collection('comments').find().toArray((err, result) => {
+    if (err) return res.sendStatus(500);
+    res.send({data: result})
+  })
+})
+
+app.post('/comments', (req, res) => {
+    console.log(req.body);
+    const { name, time, content } = req.body;
+    if( !name || !time || !content ){
+        res.sendStatus(403);
+    }
+    db.collection('comments').save(req.body, (err, result) => {
+        if (err) return res.sendStatus(500);
+        console.log('saved to database')
+        res.send(req.body);
+    });
+})
+
+app.delete('/comments/:id', (req, res) => {
+    console.log(req.params.id);
+    if(!req.params.id){
+        res.sendStatus(403);
+    }
+    var obj = {_id: ObjectID(req.params.id)};
+    db.collection("comments").remove(obj, function(err, obj) {
+        if (err) {
+            return res.sendStatus(500);
+        }
+        console.log("1 document deleted");
+        res.send('delete success');
+    });
+})
+
+app.put('/comments/:id', (req, res) => {
+    console.log(req.params.id, req.body); 
+    console.log(req.params.id);
+    if(!req.body){
+        res.sendStatus(403);
+    }
+    var newvalues = {$set: req.body};
+    var obj = {_id: ObjectID(req.params.id)};
+    db.collection("comments").updateOne(obj, newvalues, function(err, obj) {
+        if (err) return res.sendStatus(500);
+        console.log("1 document update");
+        res.send('update success');
+    });
+})
+
+const port = process.env.PORT || 3000;
+
+MongoClient.connect(url, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('ian-test')
+    app.listen(port, () => {
+        console.log('listening on 3000')
+    })
+})
